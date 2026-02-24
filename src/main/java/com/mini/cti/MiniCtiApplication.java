82a0ab26dee8b0e7ac8775762d73f74671d0555a
sprs <spiroskos9@gmail.com>
2026-02-24 19:18:58 +0200

    feat: add EnableJpaAuditing

--- src/main/java/com/mini/cti/MiniCtiApplication.java
+++ src/main/java/com/mini/cti/MiniCtiApplication.java
@@ -2,8 +2,10 @@ package com.mini.cti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MiniCtiApplication {
 
 	public static void main(String[] args) {
