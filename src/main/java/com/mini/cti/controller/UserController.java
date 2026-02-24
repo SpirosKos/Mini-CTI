24038ecb2680674fcf89fb2674c30d22ce779247
sprs <spiroskos9@gmail.com>
2026-02-24 13:43:03 +0200

    feat: add /users/uuid endpoint

--- src/main/java/com/mini/cti/controller/UserController.java
+++ src/main/java/com/mini/cti/controller/UserController.java
@@ -3,6 +3,7 @@ package com.mini.cti.controller;
 
 import com.mini.cti.core.exceptions.InvalidCredentialException;
 import com.mini.cti.core.exceptions.UserAlreadyExistsException;
+import com.mini.cti.core.exceptions.UserNotFoundException;
 import com.mini.cti.dto.ErrorResponseDTO;
 import com.mini.cti.dto.UserRequestDTO;
 import com.mini.cti.dto.UserResponseDTO;
@@ -14,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,6 +34,12 @@ public class UserController {
     @PostMapping("/users/login")
     public ResponseEntity<UserResponseDTO> loginUser(@Valid @RequestBody UserRequestDTO userRequestDTO) throws InvalidCredentialException {
         UserResponseDTO responseDTO = iUserService.loginUser(userRequestDTO);
+        return ResponseEntity.status(200).body(responseDTO);
+    }
+
+    @GetMapping("/users/{uuid}")
+    public ResponseEntity<UserResponseDTO> getUserByUUID(@PathVariable UUID uuid) throws UserNotFoundException {
+        UserResponseDTO responseDTO = iUserService.getUserByUUID(uuid);
         return ResponseEntity.status(200).body(responseDTO);
     }
 
