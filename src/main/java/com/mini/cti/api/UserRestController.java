fb945d5a2bf08cf0887b7feb8ab6e58b10134512
sprs <spiroskos9@gmail.com>
2026-02-25 19:57:53 +0200

    fix: rename

--- src/main/java/com/mini/cti/api/UserRestController.java
+++ src/main/java/com/mini/cti/api/UserRestController.java
@@ -1,19 +1,15 @@
-package com.mini.cti.controller;
+package com.mini.cti.api;
 
 
 import com.mini.cti.core.exceptions.InvalidCredentialException;
 import com.mini.cti.core.exceptions.UserAlreadyExistsException;
 import com.mini.cti.core.exceptions.UserNotFoundException;
-import com.mini.cti.dto.ErrorResponseDTO;
 import com.mini.cti.dto.UserRequestDTO;
 import com.mini.cti.dto.UserResponseDTO;
 import com.mini.cti.service.IUserService;
-import com.mini.cti.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -21,7 +17,7 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class UserController {
+public class UserRestController {
 
     private final IUserService iUserService;
 
