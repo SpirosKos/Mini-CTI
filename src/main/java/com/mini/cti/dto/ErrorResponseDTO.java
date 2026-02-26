8095bcc69970d1d5c112b56d28c0403deaab5db5
sprs <spiroskos9@gmail.com>
2026-02-26 23:39:34 +0200

    feat: modify for the new exception

--- src/main/java/com/mini/cti/dto/ErrorResponseDTO.java
+++ src/main/java/com/mini/cti/dto/ErrorResponseDTO.java
@@ -1,6 +1,8 @@
 package com.mini.cti.dto;
 
-public record ErrorResponseDTO(
-        String message,
-        int status
-) {}
+public record ErrorResponseDTO(String code, String description) {
+
+    public ErrorResponseDTO(String code){
+        this(code, "");
+    }
+}
