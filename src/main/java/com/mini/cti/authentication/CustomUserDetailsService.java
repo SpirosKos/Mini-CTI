e5cfd44b5769eecaaf4df75691608188f1de9644
sprs <spiroskos9@gmail.com>
2026-03-02 12:01:32 +0200

    feat: add user detail service for authentication

---
+++ src/main/java/com/mini/cti/authentication/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.mini.cti.authentication;
+
+import com.mini.cti.model.User;
+import com.mini.cti.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
