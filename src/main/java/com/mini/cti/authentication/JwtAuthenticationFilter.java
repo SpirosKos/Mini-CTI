6e4bd4b3fe39ae4aa0cfae55fe5fae225870d4d1
sprs <spiroskos9@gmail.com>
2026-03-02 13:14:06 +0200

    feat: add authentication filter

---
+++ src/main/java/com/mini/cti/authentication/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.mini.cti.authentication;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String jwt = extractJwtFromRequest(request);
+
+            if (jwt != null && !jwtUtil.validateToken(jwt)) {
+                String username = jwtUtil.extractUsername(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken passwordAuthenticationFilter = new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+                passwordAuthenticationFilter.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(passwordAuthenticationFilter);
+            }
+
+        }catch (Exception e){
+            logger.error("User could not be authenticated.", e);
+        }
+
+        filterChain.doFilter(request,response);
+
+    }
+
+    private String extractJwtFromRequest(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
+    }
+}
