902409df1d33027da70ab28c476108764226ba5d
sprs <spiroskos9@gmail.com>
2026-03-04 00:16:50 +0200

    feat: add declaration of data

--- src/main/java/com/mini/cti/model/IpLookupEntity.java
+++ src/main/java/com/mini/cti/model/IpLookupEntity.java
@@ -1,4 +1,39 @@
 package com.mini.cti.model;
 
-public class IpLookupEntity {
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "ip_lookup")
+public class IpLookupEntity extends AbstractEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // Basic info
+    private String ipAddress;       // 8.8.8.8
+    private String country;         // "US"
+    private String asOwner;         // "Google LLC"
+
+
+    // Reputation
+    private Integer reputation;     // 527 higher = better
+
+    // Metadata
+    private Instant analyzedAt;       // When checked
+    private Instant lastAnalysisDate;  // VirusTotals's last check
+
+    @ManyToOne
+    private User user;
+
+    private Integer malicious;      // 0
+    private Integer suspicious;     // 0
+    private Integer harmless;       // 58
+    private Integer undetected;     // 36
+
+
 }
