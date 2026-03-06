7bfb0b4939a6e0441aa7879caf77724947320067
sprs <spiroskos9@gmail.com>
2026-03-06 22:52:42 +0200

    feat: add searchedAt getter setter noargs & allargs contructor

--- src/main/java/com/mini/cti/model/UserLookUp.java
+++ src/main/java/com/mini/cti/model/UserLookUp.java
@@ -2,10 +2,19 @@ package com.mini.cti.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Table(name = "user_lookups")
 public class UserLookUp extends AbstractEntity{
 
@@ -19,7 +28,9 @@ public class UserLookUp extends AbstractEntity{
     private IpCache ipCache;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    private Instant searchedAt;     // When User searched for the IP
 
 }
