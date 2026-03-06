5c692395cdbec8200f772d6a10557220737a38a0
sprs <spiroskos9@gmail.com>
2026-03-06 23:03:13 +0200

    feat: add fields for IpLookUpResponseDTO

--- src/main/java/com/mini/cti/dto/IpLookUpResponseDTO.java
+++ src/main/java/com/mini/cti/dto/IpLookUpResponseDTO.java
@@ -1,5 +1,17 @@
-//package com.mini.cti.dto;
-//
-//
-//public record IpLookUpResponseDTO {
-//}
+package com.mini.cti.dto;
+
+
+import java.time.Instant;
+
+public record IpLookUpResponseDTO(
+        String ipAddress,
+        String country,
+        String asOwner,
+        String reputation,
+        Integer malicious,
+        Integer suspicious,
+        Integer harmless,
+        Integer undetected,
+        Instant lastAnalysisDate
+) {
+}
