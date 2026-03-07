089f6dd1fefcd0bc676eee3ca14ffaf662e99a67
sprs <spiroskos9@gmail.com>
2026-03-07 23:24:07 +0200

    feat: todo list to continue

--- src/main/java/com/mini/cti/service/IpLookUpService.java
+++ src/main/java/com/mini/cti/service/IpLookUpService.java
@@ -1,5 +1,7 @@
 package com.mini.cti.service;
 
+import com.mini.cti.dto.IpLookUpResponseDTO;
+import com.mini.cti.model.User;
 import com.mini.cti.repository.IpCacheRepository;
 import com.mini.cti.repository.UserLookUpRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +15,26 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class IpLookUpService {
 
-    private IpCacheRepository ipCacheRepository;
-    private UserLookUpRepository userLookUpRepository;
-    private Mapper mapper;
-    private VirusTotalService virusTotalService;
+    private final IpCacheRepository ipCacheRepository;
+    private final UserLookUpRepository userLookUpRepository;
+    private final Mapper mapper;
+    private final VirusTotalService virusTotalService;
+
+    public IpLookUpResponseDTO lookUpIp(String ipAddress, User user) {
+
+        // TODO  // 1. Check cache in database
+        //    // 2. Determine if cache is fresh or stale
+        //    // 3. If fresh → return from cache
+        //    // 4. If stale/missing → fetch from VirusTotal
+        //    // 5. Save/update cache
+        //    // 6. Record user search history
+        //    // 7. Map and return response
+
+
+        if (ipCacheRepository.existsById(ipAddress)) {
+
+        }
+    }
 
 
 
