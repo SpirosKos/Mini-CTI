febb912aac66f26f13206deaa49eaf2f28152ed2
sprs <spiroskos9@gmail.com>
2026-03-10 22:09:56 +0200

    feat: add virustotal exception

---
+++ src/main/java/com/mini/cti/core/exceptions/VirusTotalApiException.java
@@ -0,0 +1,8 @@
+package com.mini.cti.core.exceptions;
+
+public class VirusTotalApiException extends RuntimeException {
+
+    public VirusTotalApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
