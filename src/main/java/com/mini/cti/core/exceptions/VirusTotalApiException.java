00b18706c030fa1cd741bf1f3459fc3c38b4eb23
sprs <spiroskos9@gmail.com>
2026-03-11 00:59:29 +0200

    feat: add an extra method

--- src/main/java/com/mini/cti/core/exceptions/VirusTotalApiException.java
+++ src/main/java/com/mini/cti/core/exceptions/VirusTotalApiException.java
@@ -1,8 +1,15 @@
 package com.mini.cti.core.exceptions;
 
-public class VirusTotalApiException extends RuntimeException {
+public class VirusTotalApiException extends AppGenericException {
+
+    private static final String DEFAULT_CODE = "VIRUSTOTAL_ERROR";
+
+    public VirusTotalApiException(String message) {
+        super(DEFAULT_CODE ,message);
+    }
 
     public VirusTotalApiException(String message, Throwable cause) {
-        super(message, cause);
+        super(DEFAULT_CODE, message);
+        initCause(cause);
     }
 }
