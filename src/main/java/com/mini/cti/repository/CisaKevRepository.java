74ce53a21a0431123c3e44daff067078089ef343
sprs <spiroskos9@gmail.com>
2026-03-18 13:45:27 +0200

    feat: add cisakev repository lookup method

---
+++ src/main/java/com/mini/cti/repository/CisaKevRepository.java
@@ -0,0 +1,11 @@
+package com.mini.cti.repository;
+
+import com.mini.cti.model.CisaKev;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CisaKevRepository extends JpaRepository<CisaKev,String> {
+
+    Optional<CisaKev> findByCveId(String cveID);
+}
