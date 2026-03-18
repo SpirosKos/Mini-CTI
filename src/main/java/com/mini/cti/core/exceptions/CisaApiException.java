6e04e3eab5c1bc857f58a66ae617aedab5330282
sprs <spiroskos9@gmail.com>
2026-03-18 15:17:16 +0200

    feat: add CisaKevException handling

---
+++ src/main/java/com/mini/cti/core/exceptions/CisaApiException.java
@@ -0,0 +1,9 @@
+package com.mini.cti.core.exceptions;
+
+public class CisaApiException extends AppGenericException {
+
+
+    public CisaApiException(String code,String message) {
+        super(code,message);
+    }
+}
