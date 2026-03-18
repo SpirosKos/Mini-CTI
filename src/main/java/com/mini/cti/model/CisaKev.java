897250a7c54e6343d1e5e506bf842cdbddb3c4da
sprs <spiroskos9@gmail.com>
2026-03-18 12:33:59 +0200

    feat: remove unique column from columns

--- src/main/java/com/mini/cti/model/CisaKev.java
+++ src/main/java/com/mini/cti/model/CisaKev.java
@@ -22,21 +22,15 @@ public class CisaKev extends AbstractEntity{
     @Column(unique = true, nullable = false, updatable = false)
     private String cveID;
 
-    @Column(unique = true)
     private String vendorProject;
 
-    @Column(unique = true)
     private String product;
 
-    @Column(unique = true)
     private String vulnerabilityName;
 
-    @Column(unique = true)
     private String dataAdded;
 
-    @Column(unique = true)
     private String shortDescription;
 
-    @Column(unique = true)
     private String requiredAction;
 }
