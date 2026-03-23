f1e6223550f4a3e66a894776cc8ead90b1bf3861
sprs <spiroskos9@gmail.com>
2026-03-23 15:44:48 +0200

    chore: update sentence structure.

--- src/main/java/com/mini/cti/core/exceptions/CisaApiException.java
+++ src/main/java/com/mini/cti/core/exceptions/CisaApiException.java
@@ -2,8 +2,9 @@ package com.mini.cti.core.exceptions;
 
 public class CisaApiException extends AppGenericException {
 
+    private static final String DEFAULT_CODE = "CISA-KEV ERROR";
 
-    public CisaApiException(String code,String message) {
-        super(code,message);
+    public CisaApiException(String message) {
+        super(DEFAULT_CODE,message);
     }
 }
