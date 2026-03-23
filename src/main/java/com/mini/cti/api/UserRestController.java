17d22c0a0d18fd85456045b813414f4de6646474
sprs <spiroskos9@gmail.com>
2026-03-23 12:34:39 +0200

    feat: add OpenAPI logic

--- src/main/java/com/mini/cti/api/UserRestController.java
+++ src/main/java/com/mini/cti/api/UserRestController.java
@@ -6,9 +6,13 @@ import com.mini.cti.core.exceptions.InvalidCredentialException;
 import com.mini.cti.core.exceptions.UserAlreadyExistsException;
 import com.mini.cti.core.exceptions.UserNotFoundException;
 import com.mini.cti.core.exceptions.ValidationException;
-import com.mini.cti.dto.UserRequestDTO;
-import com.mini.cti.dto.UserResponseDTO;
+import com.mini.cti.dto.*;
 import com.mini.cti.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +37,45 @@ public class UserRestController {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
+
+
+    @Operation(
+            summary = "Register a new user",
+            description ="Creates a new user in the system")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "created successfully.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Validation error.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "User already exists.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal Server error.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody UserRequestDTO userRequestDTO,
                                                         BindingResult bindingResult) throws UserAlreadyExistsException, ValidationException, InvalidCredentialException {
@@ -56,8 +99,55 @@ public class UserRestController {
 
     }
 
+
+
+    @Operation(
+            summary = "Login to system",
+            description ="A register user can login to system.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Successfully login, returns token.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AuthResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Validation error.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Invalid credentials error.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User not found.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal Server error.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
     @PostMapping("/users/login")
-    public ResponseEntity<String> login(@RequestBody UserRequestDTO loginRequest) {
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody UserRequestDTO loginRequest) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken
                 (loginRequest.email(), loginRequest.password()));
 
@@ -68,11 +158,13 @@ public class UserRestController {
 
         String token = jwtService.generateToken(authentication.getName(), role);
 
-        return ResponseEntity.ok("{\"token\":\"" + token + "\"}");
+        return ResponseEntity.ok(new AuthResponseDTO(token));
     }
 
 
-    // TODO connect with frontend for get into user profile most probably.
+
+
+    // TODO For a future service and prevent users to inject another users UUID
     @GetMapping("/users/{uuid}")
     public ResponseEntity<UserResponseDTO> getUserByUUID(@PathVariable UUID uuid) throws UserNotFoundException {
         UserResponseDTO responseDTO = iUserService.getUserByUUID(uuid);
